18874940e5697cf351b13b2aa0b2da47475926c1
samba <sambasivavathala884@gmail.com>
2023-03-20 09:28:44 +0530

    first commit

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+msg="sambasiva welcome to python"
+
+print(msg)
+
+
+
+
+
+# ===============================================
+
+#============== OUTPUT========================
+
+# terminal cmd check output
+# .\helloworld
